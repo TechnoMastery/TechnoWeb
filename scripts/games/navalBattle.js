f6cfe9743a8d9c7a9ec4c57b05bef41c407f6cc7
Minheur2000 <adrien.rousselet@hotmail.com>
2025-04-21 17:28:36 +0200

    finished js from naval battle lobby. ready for test

--- scripts/games/navalBattle.js
+++ scripts/games/navalBattle.js
@@ -3,8 +3,19 @@ let gameName;
 let playerCount;
 document.getElementById('creatingStatus').textContent = "Game Status : Waiting for game info being subited...";
 
+function writeStatus(content) {
+    document.getElementById('creatingStatus').textContent = "Game Status : " + content;
+}
 function switchPage() {
     window.location.replace('/TechnoMastery/pages/gameHub/navalBattle/game-settings')
+}
+function status2() {
+    writeStatus("Starting game info saving")
+    setTimeout(writeStatus("Starting game info saving."), 1500);
+    setTimeout(writeStatus("Starting game info saving.."), 1500);
+    setTimeout(writeStatus("Starting game info saving..."), 1500);
+    setTimeout(writeStatus("Ready ! Transfering..."), 1000);
+    setTimeout(switchPage(), 3000);
 }
 
 function createGame() {
@@ -23,7 +34,8 @@ function createGame() {
 
     localStorage.setItem('gameInfos', JSON.stringify(gameData));
 
-    alert("Game created and saved ! Your game is being processed, you can close this alert and wait for being trasfered.");
     console.log("Game saved as 'gameData' JSON : " + gameData);
-    setTimeout(switchPage(), 2)
+    document.getElementById('creatingStatus').textContent = "Game Status : Waiting popup from being close...";
+    alert("Game created and saved ! Your game is being processed, you can close this alert and wait for being trasfered.");
+    setTimeout(status2(), 2000);
 }
