a123fcdd75791ccf72757092cc9751ddcab924a6
Minheur2000 <adrien.rousselet@hotmail.com>
2025-04-21 19:07:50 +0200

    patch 12 (untested)

--- scripts/games/navalBattle/navalBattleLobby.js
+++ scripts/games/navalBattle/navalBattleLobby.js
@@ -49,12 +49,23 @@ function createGame() {
     };
     localStorage.setItem('gameInfos', JSON.stringify(gameData));
 
-    let newGamesListJson = JSON.parse(localStorage.getItem('gamesList'));
-    newGamesListJson.gameCount = newGamesListJson.gameCount +1;
-    const newGameId = newGamesListJson.gameCount;
-    newGamesListJson[newGameId] = {
+    let newGamesListJson;
+    if(gamesListJson) {
+        newGamesListJson = JSON.parse(localStorage.getItem('gamesList'));
+        newGamesListJson.gameCount = newGamesListJson.gameCount +1;
+        const newGameId = newGamesListJson.gameCount;
+        newGamesListJson[newGameId] = {
         name: gameName,
         playerCount: playerCount
+        };
+    } else {
+        newGamesListJson = {
+            gameCount: 1,
+            "1": {
+                name: gameName,
+                playerCount: playerCount
+            }
+        }
     };
     localStorage.setItem('gamesList', JSON.stringify(newGamesListJson));
 
