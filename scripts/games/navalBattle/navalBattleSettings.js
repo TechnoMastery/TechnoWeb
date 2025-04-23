684c5b9598d39c925f4f53e8da8b66d6914954dd
Minheur2000 <adrien.rousselet@hotmail.com>
2025-04-23 17:01:03 +0200

    patched (tried) basics wrong spellings

--- scripts/games/navalBattle/navalBattleSettings.js
+++ scripts/games/navalBattle/navalBattleSettings.js
@@ -1,9 +1,10 @@
 const gameInfoJson = JSON.parse(localStorage.getItem('gameInfo'));
 const playerCount = gameInfoJson.playerCount;
-const gameName = gameInfoJson.gameName;
+const gameName = gameInfoJson.name;
 if(gameInfoJson) {
     document.getElementById('gameName').textContent = gameName;
     document.getElementById('playerCount').textContent = playerCount;
+    document.getElementById('playerCount2').textContent = playerCount;
 } else {
     alert('ERROR : No game info found. Please create one.');
     window.location.replace('/TechnoWeb/pages/gameHub/navalBattle/game-lobby')
