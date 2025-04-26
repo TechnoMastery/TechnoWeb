7281eb4acf6d59dc54615bdf5dfc8d9f0d6d9ddf
Minheur2000 <adrien.rousselet@hotmail.com>
2025-04-26 12:38:07 +0200

    patched pictures not sticking together

--- scripts/games/navalBattle/gameBoard.js
+++ scripts/games/navalBattle/gameBoard.js
@@ -32,25 +32,20 @@ function saveNewDatas(enableReloadPage, gameStatus) {
         window.location.reload();
     };
 };
-document.getElementById("gameName").textContent = gameName;
+document.getElementById("gameName").textContent = gameName
 function fillSeaGrid() {
     // empty grid
     seaBoard.innerHTML = "";
-    // this "for" is creating lines. 10 lignes per player
-    for(let i=1; i <= (playerCount * 10); i++) {
-        // this "for" is creating 10 pictures per line
-        for(let j=1; j <= 10; j++) {
-            const seaLineItem;
-            seaLineItem.innerHTML = `
-                <img src="/TechnoWeb/ressources/pictures/navalBattle/sea/sea.png" alt="sea">
-            `;
-            seaBoard.append(seaLineItem);
-        };
-        const seaLineBR = document.createElement('span');
-        seaLineBR.innerHTML = `
-            <br>
-        `;
-        seaBoard.append(seaLineBR);
-    };
+    seaBoard.style.display = "grid";
+    seaBoard.style.gridTemplateColumns = "repeat(10, 32px)";
+    seaBoard.style.width = "fit-content";
+    for (let i = 1; i <= (playerCount * 10 * 10); i++) {
+        const seaTile = document.createElement('img');
+        seaTile.src = "/TechnoWeb/ressources/pictures/navalBattle/sea/sea.png";
+        seaTile.alt = "sea";
+        seaTile.style.width = "64px";
+        seaTile.style.height = "64px";
+        seaBoard.append(seaTile);
+    }
 };
 fillSeaGrid();
