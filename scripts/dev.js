9fc5a9c996e5b9f82ebd41a630531242858dfb10
Minheur2000 <adrien.rousselet@hotmail.com>
2025-07-19 17:08:10 +0200

    d

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,6 +1,8 @@
 const devDiv = document.getElementById('dev');
 const devData = JSON.parse(localStorage.getElementById('devData'));
-if(devData) {
+if(devData.devMod == true) {
+
+console.log("dev mod enabled !")
     devDiv.innerHTML = `
         <a href="pages/dev"><button class="buttons button-blue">DEV only</button></a><br>
     `
