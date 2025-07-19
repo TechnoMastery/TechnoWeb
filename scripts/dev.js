ebac77673300d8293b5daf4a418b8241cf1476c0
Minheur2000 <adrien.rousselet@hotmail.com>
2025-07-19 17:05:33 +0200

    d

---
+++ scripts/dev.js
@@ -0,0 +1,7 @@
+const devDiv = document.getElementById('dev');
+const devData = JSON.parse(localStorage.getElementById('devData'));
+if(devData) {
+    devDiv.innerHTML = `
+        <a href="pages/dev"><button class="buttons button-blue">DEV only</button></a><br>
+    `
+}
